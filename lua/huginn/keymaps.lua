97ac90a8a3db6744e1f0dc35ab3e9139cbd6ef71
Oleg Burenkov <burenkov.oleg@gmail.com>
2026-09-19 00:50:50 +0300

    feat: make test commands configuration driven

--- lua/huginn/keymaps.lua
+++ lua/huginn/keymaps.lua
@@ -1,28 +1,73 @@
+local config = require("huginn.config")
+
 local M = {}
 
-local function poetry_pytest(args)
-  local command = { "poetry", "run", "pytest" }
+local function split_args(value)
+  return vim.split(value, "%s+", { trimempty = true })
+end
+
+local function command_prefix()
+  local manager = config.get().python.package_manager
+
+  if manager == "poetry" then
+    return { "poetry", "run" }
+  elseif manager == "uv" then
+    return { "uv", "run" }
+  elseif manager == "pipenv" then
+    return { "pipenv", "run" }
+  elseif manager == "none" or manager == "" then
+    return {}
+  end
+
+  return { manager }
+end
+
+local function run_command(args)
+  local command = command_prefix()
   for _, arg in ipairs(args or {}) do
     table.insert(command, arg)
   end
+
   vim.cmd("botright split | terminal " .. table.concat(vim.tbl_map(vim.fn.shellescape, command), " "))
+end
+
+local function run_profile(name)
+  local cfg = config.get()
+  local profile = cfg.testing.profiles[name]
+
+  if not profile then
+    vim.notify(("Huginn: unknown pytest profile '%s'"):format(name), vim.log.levels.ERROR)
+    return
+  end
+
+  run_command(vim.tbl_extend("force", { cfg.testing.runner }, profile))
 end
 
 function M.setup()
   vim.keymap.set("n", "<leader>tt", function()
-    poetry_pytest({ "tests" })
-  end, { desc = "Run all tests" })
+    run_profile("default")
+  end, { desc = "Run default test profile" })
 
   vim.keymap.set("n", "<leader>tf", function()
-    poetry_pytest({ vim.fn.expand("%:p") })
+    run_command({ config.get().testing.runner, vim.fn.expand("%:p") })
   end, { desc = "Run current test file" })
 
+  vim.keymap.set("n", "<leader>tp", function()
+    local names = vim.tbl_keys(config.get().testing.profiles)
+    table.sort(names)
+    vim.ui.select(names, { prompt = "Test profile" }, function(name)
+      if name then
+        run_profile(name)
+      end
+    end)
+  end, { desc = "Run test profile" })
+
   vim.keymap.set("n", "<leader>ta", function()
-    local args = vim.fn.input("pytest args: ")
+    local args = vim.fn.input("test args: ")
     if args ~= "" then
-      poetry_pytest(vim.split(args, " ", { trimempty = true }))
+      run_command(vim.tbl_extend("force", { config.get().testing.runner }, split_args(args)))
     end
-  end, { desc = "Run pytest with arguments" })
+  end, { desc = "Run tests with arguments" })
 
   vim.keymap.set("n", "<leader>tr", function()
     require("neotest").run.run()
