53f70559e401bed593d2f6a722d4be9461f07379
Oleg Burenkov <burenkov.oleg@gmail.com>
2026-09-19 02:17:18 +0300

    fix: make top-level Huginn setup idempotent

--- lua/huginn/init.lua
+++ lua/huginn/init.lua
@@ -1,8 +1,14 @@
 local config = require("huginn.config")
 
 local M = {}
+local setup_done = false
 
 function M.setup()
+  if setup_done then
+    return
+  end
+  setup_done = true
+
   config.setup()
   require("huginn.lazy")
   require("huginn.keymaps").setup()
